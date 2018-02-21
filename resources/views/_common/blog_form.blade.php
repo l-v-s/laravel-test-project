25bc6f820b62225eaca1d716edbeb9a6fc77e8b6
l-v-s <l-v-s@ukr.net>
2018-02-21 10:52:13 +0200

    21.02 19:51 блог работает и 2 разные формы для добавления и редактирования блога

---
+++ resources/views/_common/blog_form.blade.php
@@ -0,0 +1,26 @@
+
+<form action="/post" method="post">
+    {{csrf_field()}}
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input class="form-control" type="text" name="title" id="title">
+    </div>
+
+    <div class="form-group">
+        <label for="alias">Alias</label>
+        <input class="form-control" type="text" name="alias" id="alias">
+    </div>
+
+    <div class="form-group">
+        <label for="intro">Intro</label>
+        <textarea class="form-control" type="text" name="intro" id="intro"></textarea>
+    </div>
+
+    <div class="form-group">
+        <label for="body">Body</label>
+        <textarea class="form-control" type="text" name="body" id="body"></textarea>
+    </div>
+
+    <div class="form-group">
+        <button class="btn btn-primary" type="submit">Post</button>
+    </div>
